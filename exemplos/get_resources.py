13c739f64803160dd6ce58d18dabf200d2833921
Lucas Kotres <lucaskotres@gmail.com>
2020-02-28 10:49:28 -0300

    atualização do exemplo get_resources.py

--- exemplos/get_resources.py
+++ exemplos/get_resources.py
@@ -3,19 +3,19 @@ import epmwebapi as epm
 
 
 @epr.applicationMethod('GetPortalResource')
-def get_portal_resource(session, filename, connection, filetype='Binary'):
+def get_portal_resource(resource_name, connection):
     """
     Get Portal resource from EPM WebServer
     """
     epmResourceManager = connection.getPortalResourcesManager()
-    file = epResourceManager.getResource(filename)
+    file = epmResourceManager.getResource(resource_name)
 
 
 @epr.applicationMethod('GetProcessorResource')
-def get_processor_resource(session, filename, connection):
+def get_processor_resource(resource_name, connection):
     """ 
     Get Processor resource from EPM WebServer
     """
     
     epResourceManager = connection.getProcessorResourcesManager()
-    file = epResourceManager.getResource(filename)
+    file = epResourceManager.getResource(resource_name)
