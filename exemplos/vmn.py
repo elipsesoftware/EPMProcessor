c9f56fb9ca27c39e5b0d0fa5227d704cfeebc0f2
Samir Lima <39410404+samirpl@users.noreply.github.com>
2022-04-05 15:50:55 -0300

    Cálculo de Vazão Mínima Noturna
    Este exemplo mostra como calcular, a partir de um evento diário, a vazão mínima noturna referente a medidores de vazão que possui uma propriedade chamada "Flow" que retorna a vazão medida e uma propriedade chamada MNF (Minimum Night Flow) onde será escrito o valor obtido.

--- exemplos/vmn.py
+++ exemplos/vmn.py
@@ -11,6 +11,15 @@ import datetime
 
 @epr.applicationMethod('Get_VMN')
 def get_vmn(session, flowMeters):
+  '''
+    Este método pressupoe que será chamado a partir de um evento diário do EPM, executado depois das 6h.
+    Sempre que o evento for executado, será passado automaticamente o objeto session, contendo informações do momento de execução do evento.
+    O segundo parâmetro é o flowMeters, que deve ser passado como um objeto "epmobjectDict" com Type sendo "FlowMeter".
+    A partir disso, percebe-se que deve existir um modelo no EPM contendo objetos do tipo FlowMeter (medidores de vazão), que contenham
+    duas propriedades:
+      1- Flow: medida que retorna a vazão medida no campo pelo medidor de vazão;
+      2- MNF: propriedade criada para receber o valor da vazão mínima medida entre o período entre 0h e 6h do dia.
+  '''
   today = session.timeEvent
   end_date = datetime.datetime(today.year,today.month,today.day,6,0)
   ini_date = datetime.datetime(today.year,today.month,today.day,0,0)
